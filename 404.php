3de8e7c9490a94108e28c46654e8559aa97af0c8
Dennis Liu <delta2six@gmail.com>
2018-06-26 15:59:52 +0100

    Add 404 page customisations

--- 404.php
+++ 404.php
@@ -13,43 +13,17 @@ get_header();
 	<div id="primary" class="content-area l-container">
 		<main id="main" class="site-main l-container">
 
-			<section class="error-404 not-found l-container--wide">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'evelyn' ); ?></h1>
+			<section class="error-404 not-found l-container--content text-center">
+				<header class="page-header ">
+					<h1 class="page-title"><?php esc_html_e( 'Oops! 404 error. ', 'evelyn' ); ?></h1>
+					<h3 class="subheading"><?php esc_html_e( 'That page can&rsquo;t be found.', 'evelyn' ); ?></h3>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'evelyn' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'evelyn' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$evelyn_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'evelyn' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$evelyn_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
+					<p><?php esc_html_e( 'It looks like nothing was found at this location.', 'evelyn' ); ?></p>
+					<div>
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="btn btn__cta btn__raised">Return Home</a>
+					</div>
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
